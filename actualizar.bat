cbfb410431738891c0d5d9fbd08517105d3a7127
mafe161200 <97056185+mafe161200@users.noreply.github.com>
2026-08-20 15:29:39 -0500

    lIMPIANDOA RCHIVOS INNECESARIOS

--- actualizar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Iniciando actualizacion automatica de arriendos...
-cd /d "D:\Computador Maf E\Escritorio\Arriendos cali"
-python scraper.py
-python normalizador.py
-echo ¡Actualizacion finalizada con exito!
